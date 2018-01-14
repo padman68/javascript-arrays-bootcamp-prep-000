1a844413d6ea83ae4472d4fe8614b15f4a79d816
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-14 02:21:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
+// function destructivelyAddElementToBeginningOfArray(array, element) {
   code to be executed
 }
 
