4bc19826c59de3a1bf14385d6d30b78f4860fb0d
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-14 02:27:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,10 +29,10 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-// function destructivelyRemoveElementFromEndOfArray(array) {
-//  array.pop()
-//  return array
-//}
+function destructivelyRemoveElementFromEndOfArray(array) {
+array.pop()
+return array
+}
 
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
