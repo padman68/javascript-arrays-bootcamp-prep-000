30cd295a0c9204fcf0480d8e4ad96df6b12684ff
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-14 02:02:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@
 var chocolateBars = ('snickers', 'hundred grand', 'kitkat', 'skittles')
 
 function addElementToBeginningOfArray(array, element) {
-  code to be executed
+  chocolateBars.unshift("mars")
 }
 
 
